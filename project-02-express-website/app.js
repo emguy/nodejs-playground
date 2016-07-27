c37c3a92461a10c021a36db92c9f8336f1c95d28
emguy <emguy.yuzhang@gmail.com>
2016-07-26 22:41:03 -0400

    More bootstrap components are added.

--- project-02-express-website/app.js
+++ project-02-express-website/app.js
@@ -3,13 +3,20 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
 
-let app = express();
 const PORT = 1337;
+
+let app = express();
+
+/* tell express about the view engine */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('<h1>Hello world</h1>'));
+app.get('/', (req, res) => res.render('index', {title: 'my title'}));
+app.get('/about', (req, res) => res.render('about', {title: 'my about title'}));
 
 app.listen(PORT);
 console.log('Server is running on port '.concat(PORT));
