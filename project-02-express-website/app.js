4ce573a478e5d5830e87e9fecfe7129d16e7d86e
emguy <emguy.yuzhang@gmail.com>
2016-07-27 22:40:09 -0400

    Messaging functionalities are added.

--- project-02-express-website/app.js
+++ project-02-express-website/app.js
@@ -17,6 +17,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index', {title: 'my title'}));
 app.get('/about', (req, res) => res.render('about', {title: 'my about title'}));
+app.get('/contact', (req, res) => res.render('contact', {title: 'my contact title'}));
+
+app.post('/contact/send', (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'emguy2000@gmail.com',
+      pass: ''
+    },
+  });
+
+  let  mailOptions = {
+    from: 'Yu Zhang <emguy2000@gmail.com>',
+    to: 'emguy2000@gmail.com',
+    subject: 'website submission',
+    text: 'You have a sumission with the following details ... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message, 
+    html: '<p> You have a sumission with the following details ... </p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.redirect('/');
+    }
+  });  
+
+});
 
 app.listen(PORT);
 console.log('Server is running on port '.concat(PORT));
