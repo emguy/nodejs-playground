8971ed6062d4ed977f66faf196ea2dda867bc3c2
emguy <emguy.yuzhang@gmail.com>
2016-08-07 23:34:06 -0400

    File uploading and form validation mechanism is added to the user registration system.

--- project-03-mongodb-auth/routes/users.js
+++ project-03-mongodb-auth/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-i/mport multer from 'multer';
+import multer from 'multer';
 let upload = multer({ dest: './uploads' });
 
 let router = express.Router();
@@ -17,14 +17,39 @@ router.get('/login', (req, res, next) => {
 });
 
 router.post('/register', upload.single('profileimage'),  (req, res, next) => {
-  //console.log('------------------');
   let name = req.body.name;
   let email = req.body.email;
   let username = req.body.username;
   let password = req.body.password;
   let password2 = req.body.password2;
 
-  console.log(req.file);
+  if(req.file) {
+  	console.log('Uploading File...');
+  	let profileimage = req.file.filename;
+  } else {
+  	console.log('No File Uploaded...');
+  	let profileimage = 'noimage.jpg';
+  }
+
+  /* form validation */
+  req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email is not valid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+  /* Check Errors */
+  let errors = req.validationErrors();
+
+  if(errors) {
+  	res.render('register', {
+  		errors: errors
+  	});
+  } else {
+  	console.log('No Errors');
+  }
 });
 
 export { router as default };
+
